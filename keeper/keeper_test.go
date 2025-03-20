f3030806e71946b3d644f72675dc3866205ff19e
stepit <stefano@noble.xyz>
2025-03-20 10:10:43 +0100

    chore: make all

--- keeper/keeper_test.go
+++ keeper/keeper_test.go
@@ -23,8 +23,9 @@ package keeper_test
 import (
 	"testing"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"autocctp.dev/types"
 	"autocctp.dev/utils"
