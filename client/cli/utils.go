bf644b17c426474ca671b05c62d5ceeb36335aa1
stepit <48993133+0xstepit@users.noreply.github.com>
2025-03-31 09:43:12 +0200

    Update client/cli/utils.go
    Co-authored-by: John Letey <j@letey.de>

--- client/cli/utils.go
+++ client/cli/utils.go
@@ -84,7 +84,7 @@ func ValidateDestinationDomain(destinationDomain string) (Domain, error) {
 }
 
 // parseAddress parses an encoded address into a 32 length byte array. If the encoded bytes are
-// less than 32, the function left pad with zeros to obtain the length used in cross-chain transfers.
+// less than 32, the function left pads to obtain the length used in cross-chain transfers.
 func (d Domain) parseAddress(address string) ([]byte, error) {
 	var bz []byte
 	switch d {
