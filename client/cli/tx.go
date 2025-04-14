36bfbc4d0fe1843251d713ddc27db0dad112097f
stepit <stefano@noble.xyz>
2025-04-14 10:43:16 +0200

    fix: update cli use description

--- client/cli/tx.go
+++ client/cli/tx.go
@@ -93,7 +93,7 @@ func TxRegisterAccount() *cobra.Command {
 
 func TxRegisterAccountSignerlessly() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "register-account-signerlessly [destination-domain] [mint-recipient] (destination-caller)",
+		Use:   "register-account-signerlessly [destination-domain] [mint-recipient] [fallback-recipient] (destination-caller)",
 		Short: "Signerlessly register an AutoCCTP account for a destination domain, a mint recipient, and a fallback recipient",
 		Long: `Signerlessly register an AutoCCTP account for a destination domain, a mint recipient, and a fallback recipient, with an optional destination caller.
 		A signerless registration does not require an existing wallet because no signature is required.`,
